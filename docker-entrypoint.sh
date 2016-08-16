e581cb602d753e2683f276499d5704bc43518dae
burcinakalin <brcnakalin@gmail.com>
2016-08-16 18:12:35 +0300

    User and password set.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,5 +18,30 @@ sed -i "s/LOGGING_SILENT/${LOGGING_SILENT}/g" /$KIBANA_CONF_DIR/kibana.yml
 sed -i "s/LOGGING_QUIET/${LOGGING_QUIET}/g" /$KIBANA_CONF_DIR/kibana.yml
 sed -i "s/LOGGING_VERBOSE/${LOGGING_VERBOSE}/g" /$KIBANA_CONF_DIR/kibana.yml
 
+if [ ! -z  ${ELASTICSEARCH_USER} ]
+then
+	sed -i "s/#elasticsearch.username/elasticsearch.username/g" /$KIBANA_CONF_DIR/kibana.yml
+	sed -i "s/ELASTICSEARCH_USER/${ELASTICSEARCH_USER}/g" /$KIBANA_CONF_DIR/kibana.yml
+	
+	if [ -z ${ELASTICSEARCH_PASS} ]
+	then
+		echo "password gir"
+		exit 1
+	fi
+
+fi
+
+if [ ! -z ${ELASTICSEARCH_PASS} ]
+then
+	sed -i "s/#elasticsearch.password/elasticsearch.password/g" /$KIBANA_CONF_DIR/kibana.yml
+	sed -i "s/ELASTICSEARCH_PASS/${ELASTICSEARCH_PASS}/g" /$KIBANA_CONF_DIR/kibana.yml
+
+        if [ -z ${ELASTICSEARCH_USER} ]
+        then
+                echo "user girmediniz"
+		exit  1
+	fi
+
+fi
 
 exec "$@"
